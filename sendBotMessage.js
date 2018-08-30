d279742a0982d921baa34804cac122a8e56a40a8
michaelZoomDA <39344486+michaelZoomDA@users.noreply.github.com>
2018-08-30 16:15:24 -0700

    Update sendBotMessage.js

--- sendBotMessage.js
+++ sendBotMessage.js
@@ -2,26 +2,21 @@ const express = require('express')
 const app = express()
 const config = require('./config');
 const request = require('request');
-
-app.get('/', (req, res) => {
-
-        //STEP 1 -  For Server to Server Integration
-        // Send bot message
+ app.get('/', (req, res) => {
+     //this will check if the code parameter is in the url, if not the most likely case is that this is the user's inital visit to oauth and we need to redirect them (Step 1)
+    //if there is a code, it most likely means they were redirected here from zoom oauth screen
+     if (req.query.code) {
+         // Send bot message
         let url = 'https://zoom.us/oauth/token?grant_type=client_credentials&client_id=' + config.clientID + '&client_secret=' + config.clientSecret;
-
-        //STEP 2
+         //STEP 3
         //we need to exchange the code for a oauth token
         request.post(url, function (error, response, body) {
-
-            //the response should be a JSON payload
+             //the response should be a JSON payload
             body = JSON.parse(body);
-
-            //get refresh token
+             //get refresh token
             let refresh_token = body.refresh_token
-
-            if (body.access_token) {
-
-                //STEP 3 
+             if (body.access_token) {
+                 //STEP 4 
                 //we can now use the access token to make API calls
                 /* 
                     Send Bot Message /POST v2/im/chat/messages 
@@ -40,23 +35,20 @@ app.get('/', (req, res) => {
                                 },
                                 json: true 
                             };
-
-                        request(options, function (error, response, body) {
+                         request(options, function (error, response, body) {
                           if (error) throw new Error(error);
-
-                          console.log(body);
+                           console.log(body);
                         });
-
-            
+             
             } else {
                 //handle error, something went wrong
                  console.log('something went wrong')
             }
-
-        }).auth(config.clientID, config.clientSecret);
-
-        return;
+         }).auth(config.clientID, config.clientSecret);
+         return;
+    }
+     //STEP 2
+    //no code provided, so redirect the user to get the code
+    res.redirect('https://zoom.us/oauth/authorize?response_type=code&client_id=' + config.clientID + '&redirect_uri=' + config.redirectUrl);
 });
-
-app.listen(3000, () => console.log('Zoom chat bot sample app listening on port 3000!'))
- 
+ app.listen(3000, () => console.log('Zoom chat bot sample app listening on port 3000!'))

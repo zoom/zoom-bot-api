5773ecc046ea5d2c5835cc0902f25917c9cf4545
michaelZoomDA <39344486+michaelZoomDA@users.noreply.github.com>
2018-09-18 11:13:44 -0700

    Removed Refresh token
    Removed Refresh token since its not needed to for client credentials.

--- sendBotMessage.js
+++ sendBotMessage.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const config = require('./config');
 const request = require('request');
+
  app.get('/', (req, res) => {
      //this will check if the code parameter is in the url, if not the most likely case is that this is the user's inital visit to oauth and we need to redirect them (Step 1)
     //if there is a code, it most likely means they were redirected here from zoom oauth screen
@@ -13,8 +14,7 @@ const request = require('request');
         request.post(url, function (error, response, body) {
              //the response should be a JSON payload
             body = JSON.parse(body);
-             //get refresh token
-            let refresh_token = body.refresh_token
+      
              if (body.access_token) {
                  //STEP 4 
                 //we can now use the access token to make API calls
